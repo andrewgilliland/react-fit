9929e638a63bce36b2ce0cffd45f97a831987549
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-03-02 20:08:42 -0600

    adds tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,35 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>React-Fit</h1>
+        <div className="font-mono border-2 border-black shadow-offset-green">
+          <div className="bg-black p-6">
+            <h1 className="text-2xl text-white font-bold">React-Fit</h1>
 
-        <p className={styles.description}>
-          Current user:
-          <code>{auth.user ? ` ${auth.user.email}` : 'none'}</code>
-        </p>
-
-        {auth.user ? (
-          <button onClick={(e) => auth.signout()}>Sign Out</button>
-        ) : (
-          <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
-        )}
+            <p className="text-white font-bold mt-2">
+              Current user:
+              <code className="text-sm text-green-400">
+                {auth.user ? ` ${auth.user.name}` : 'none'}
+              </code>
+            </p>
+          </div>
+          <div className="p-6">
+            {auth.user ? (
+              <button
+                className="bg-green-400 px-8 py-2 text-black border border-black shadow-offset-black mt-3"
+                onClick={(e) => auth.signout()}
+              >
+                Sign Out
+              </button>
+            ) : (
+              <button
+                className="bg-green-300 px-8 py-2 text-black border border-black shadow-offset-black mt-3"
+                onClick={(e) => auth.signinWithGitHub()}
+              >
+                Sign In
+              </button>
+            )}
+          </div>
+        </div>
       </main>
 
       <footer className={styles.footer}>
