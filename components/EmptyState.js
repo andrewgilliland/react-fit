7d221288a4f92b1abc860ca30578263a34da5e15
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-04-01 09:18:56 -0500

    updates account page

--- components/EmptyState.js
+++ components/EmptyState.js
@@ -19,14 +19,20 @@ const EmptyState = () => {
       <DashboardShell>
         <h1 className="font-mono font-semibold text-2xl mt-10"> My Account</h1>
         <div className="bg-black shadow-offset-green p-6 mt-6">
-          <h2 className="font-xl font-semibold text-white">
-            Email:{' '}
-            <span className="text-green-400">{user.email}</span>
-          </h2>
-          <h2 className="font-xl font-semibold text-white">
+          {user.name ? (
+            <div className="font-xl font-semibold text-white">
+              Name: <span className="text-green-400">{user.name}</span>
+            </div>
+          ) : (
+            <></>
+          )}
+          <div className="font-xl font-semibold text-white mt-2">
+            Email: <span className="text-green-400">{user.email}</span>
+          </div>
+          <div className="font-xl font-semibold text-white mt-2">
             Current Plan:{' '}
             <span className="text-green-400">{user.stripeRole}</span>
-          </h2>
+          </div>
         </div>
         <div className="font-mono border-2 border-black shadow-offset-green mt-5 px-10 py-4">
           <h3 className="font-semibold mb-2">Dont't have a membership?</h3>
@@ -34,7 +40,7 @@ const EmptyState = () => {
 
           <Link href="/membership">
             <a className="bg-green-400 px-8 py-2 text-black border border-black shadow-offset-black hover:bg-green-500">
-              Upgrade Membership
+              Start Membership
             </a>
           </Link>
         </div>
