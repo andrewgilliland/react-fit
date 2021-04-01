95ac389a9e729e7964d5e8027cf884ac879ecc81
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-04-01 09:52:30 -0500

    updates workout route restrictions

--- pages/workouts/standard.js
+++ pages/workouts/standard.js
@@ -1,53 +1,64 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import useSWR from 'swr';
 
+import { goToBillingPortal } from '@/lib/db';
 import { useAuth } from '@/lib/auth';
+import Button from '@/components/Button';
 import DashboardShell from '@/components/DashboardShell';
 import WorkoutCard from '@/components/WorkoutCard';
 
 export default function Standard() {
   const { user } = useAuth();
+  const [isBillingLoading, setBillingLoading] = useState(false);
 
-  if (user.stripeRole === 'standard' || 'warrior') {
+  if (user.stripeRole === 'free') {
     return (
-      <div>
-        <Head>
-          <title>Standard Workouts</title>
-        </Head>
-        <DashboardShell>
-          <h2 className="font-mono font-semibold text-2xl mt-10">
-            Standard Workouts
-          </h2>
-          <section className="grid grid-cols-3 gap-10 mt-6">
-            <WorkoutCard
-              name="Upper Body Blast"
-              date="4/1/21"
-              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste perferendis soluta in expedita tenetur possimus?"
-            />
-            <WorkoutCard
-              name="Loaded Legs"
-              date="4/3/21"
-              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste perferendis soluta in expedita tenetur possimus?"
-            />
-            <WorkoutCard
-              name="Insane Abs"
-              date="4/5/21"
-              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste perferendis soluta in expedita tenetur possimus?"
-            />
-            
-          </section>
-        </DashboardShell>
-      </div>
+      <DashboardShell>
+        <h1 className="font-mono font-semibold text-xl mt-10">
+          Upgrade your membership to view this awesome content.
+        </h1>
+        <Button
+          isLoading={isBillingLoading}
+          onClick={(e) => {
+            setBillingLoading(true);
+            goToBillingPortal();
+          }}
+        >
+          Upgrade Membership
+        </Button>
+      </DashboardShell>
     );
   }
 
   return (
-    <DashboardShell>
-      <h2 className="font-mono font-semibold text-2xl mt-10">
-        Unauthorized Content
-      </h2>
-      <p>Please go back to your Account page to upgrade your membership.</p>
-    </DashboardShell>
+    <div>
+      <Head>
+        <title>Standard Workouts</title>
+      </Head>
+      <DashboardShell>
+        <h1 className="font-mono font-semibold text-2xl mt-10">
+          Standard Workouts
+        </h1>
+        <section className="grid grid-cols-3 gap-10 mt-6">
+          <WorkoutCard
+            name="Upper Body Blast"
+            date="4/1/21"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste perferendis soluta in expedita tenetur possimus?"
+          />
+          <WorkoutCard
+            name="Loaded Legs"
+            date="4/3/21"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste perferendis soluta in expedita tenetur possimus?"
+          />
+          <WorkoutCard
+            name="Insane Abs"
+            date="4/5/21"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste perferendis soluta in expedita tenetur possimus?"
+          />
+        </section>
+      </DashboardShell>
+    </div>
   );
 }
--- pages/workouts/warrior.js
+++ pages/workouts/warrior.js
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
 import useSWR from 'swr';
 
-import { useAuth } from '@/lib/auth';
 import DashboardShell from '@/components/DashboardShell';
+import Button from '@/components/Button';
 import WorkoutCard from '@/components/WorkoutCard';
+import { useAuth } from '@/lib/auth';
+import { goToBillingPortal } from '@/lib/db';
 
 export default function Warrior() {
   const { user } = useAuth();
+  const [isBillingLoading, setBillingLoading] = useState(false);
   //   const { data } = useSWR('/api/workouts', fetcher);
 
   //   if (!data) {
@@ -21,10 +24,18 @@ export default function Warrior() {
   if (user.stripeRole !== 'warrior') {
     return (
       <DashboardShell>
-        <h2 className="font-mono font-semibold text-2xl mt-10">
-          Unauthorized Content
-        </h2>
-        <p>Please go back to your Account page to upgrade your membership.</p>
+        <h1 className="font-mono font-semibold text-xl mt-10">
+          Upgrade your membership to view this awesome content.
+        </h1>
+        <Button
+          isLoading={isBillingLoading}
+          onClick={(e) => {
+            setBillingLoading(true);
+            goToBillingPortal();
+          }}
+        >
+          Upgrade Membership
+        </Button>
       </DashboardShell>
     );
   }
@@ -35,9 +46,9 @@ export default function Warrior() {
         <title>Warrior Workouts</title>
       </Head>
       <DashboardShell>
-        <h2 className="font-mono font-semibold text-2xl mt-10">
+        <h1 className="font-mono font-semibold text-2xl mt-10">
           Warrior Workouts
-        </h2>
+        </h1>
         <section className="grid grid-cols-3 gap-10 mt-6">
           <WorkoutCard
             name="Chest Annihilation"
